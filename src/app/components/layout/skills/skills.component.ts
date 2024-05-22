f00347779d146c6eb28a96be500d83624e79cd48
andres gomez cadavid <cangoc180@gmail.com>
2024-05-21 19:42:34 -0500

    add img and section experience

--- src/app/components/layout/skills/skills.component.ts
+++ src/app/components/layout/skills/skills.component.ts
@@ -14,11 +14,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 export class SkillsComponent {
 
   public images = [
-    { src: "../../../../assets/img-skills/angular.webp", alt: "Angular", label:"Angular" },
-    { src: "../../../../assets/img-skills/logo-vue.png", alt: "Vue", label:"Vue" },
-    { src: "../../../../assets/img-skills/icon-nodejs.e9fdb7cb.svg", alt: "Node.js", label:"Node.js" },
-    { src: "../../../../assets/img-skills/icon-javascript.d5945e90.svg", alt: "JavaScript", label:"JavaScript" },
-    { src: "../../../../assets/img-skills/icon-typescript.0f2fa2a9.svg", alt: "TypeScript", label:"TypeScript" }
+    { src: "./assets/img-skills/angular.webp", alt: "Angular", label:"Angular" },
+    { src: "./assets/img-skills/logo-vue.png", alt: "Vue", label:"Vue" },
+    { src: "./assets/img-skills/icon-nodejs.e9fdb7cb.svg", alt: "Node.js", label:"Node.js" },
+    { src: "./assets/img-skills/icon-javascript.d5945e90.svg", alt: "JavaScript", label:"JavaScript" },
+    { src: "./assets/img-skills/icon-typescript.0f2fa2a9.svg", alt: "TypeScript", label:"TypeScript" }
   ];
 
 }
